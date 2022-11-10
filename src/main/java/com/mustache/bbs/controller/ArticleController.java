044e0c2b803c59ef534a799bac5fd6a5796e4fc9
sangho527 <sy980527@naver.com>
2022-11-10 17:53:16 +0900

    show, error, eidt, list 추가 221110

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -6,15 +6,20 @@ import com.mustache.bbs.domain.entity.Article;
 import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
 @Slf4j
 public class ArticleController {
-
+    // 스프링이 ArticleRepository 구현체를 DI 해줌
     private final ArticleRepository articleRepository;
 
     public ArticleController(ArticleRepository articleRepository) {
@@ -22,9 +27,28 @@ public class ArticleController {
     }
 
 
-    @GetMapping(value = "/new")
-    public String newArticleForm() {
-        return "articles/new";
+//    @GetMapping(value = "/new")
+//    public String newArticleForm() {
+//
+//        return "articles/new";
+//    }
+
+    @GetMapping("/new")
+    public String createPage() {
+        return "new";
+    }
+
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+
+        if (!optArticle.isEmpty()) {
+            // Optional.get() ---> Article
+            model.addAttribute("article", optArticle.get());
+            return "show";
+        } else {
+            return "error";
+        }
     }
 
     @PostMapping(value = "/posts")
--- src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -1,27 +1,25 @@
 package com.mustache.bbs.domain.dto;
 
 import com.mustache.bbs.domain.entity.Article;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
+@AllArgsConstructor
 @ToString
 public class ArticleDto {
     private Long id;
     private String title;
     private String content;
 
-    public ArticleDto(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
+//    public ArticleDto(Long id, String title, String content) {
+//        this.id = id;
+//        this.title = title;
+//        this.content = content;
+//    }
 
     public Article toEntity() {
         return new Article(title, content);
     }
-
-//    public ArticleDto(String title) {
-//        this.title = title;
-//    }
 }
